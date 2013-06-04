ee405dca8fa0e661f6fea1019f3b6f0e84fd16c1
The Software Bin <andrear1979@hotmail.com>
2013-06-05 02:23:20 +0300

    Create extract-string-from-webpage.el

---
+++ snippets/extract-string-from-webpage.el
@@ -0,0 +1,18 @@
+(defun tsb/extract-string-from-webpage (pageurl regexp regexp-group-nr)
+  "Retrieve HTTP page at address `pageurl', 
+   search for first occurrence of `regexp',
+   return its group `regexp-group-nr' as
+   a string."
+  (let 
+      (
+       (b (url-retrieve-synchronously (url-generic-parse-url pageurl)))
+       (previous-buffer (current-buffer))
+       (s))
+    (progn
+      (set-buffer b)
+      (beginning-of-buffer)
+      (re-search-forward regexp)
+      (setq s (match-string regexp-group-nr))
+      (set-buffer previous-buffer)
+      (kill-buffer b)
+      s)))
